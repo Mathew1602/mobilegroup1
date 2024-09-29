f42b5b4656fd16e1040c3e96e6a035a1d3efad85
xiaoyazou <zouxiao@shernet.sheridancollege.ca>
2024-09-29 12:56:58 -0400

    Initial commit by xiaoya

--- GroupProjectIOS/ContentView.swift
+++ GroupProjectIOS/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Group Members :)")
+            Text("Hello! Xiaoya is here")
         }
         .padding()
     }
