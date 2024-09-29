5f4712ede03d7d23cb799a032a9b79c2bbb4c53a
Mathew <mattboyd1602@gmail.com>
2024-09-29 10:53:37 -0400

    Initial Commit

---
+++ GroupProjectIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroupProjectIOS
+//
+//  Created by Mathew Boyd on 2024-09-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroupProjectIOS/GroupProjectIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupProjectIOSApp.swift
+//  GroupProjectIOS
+//
+//  Created by Mathew Boyd on 2024-09-29.
+//
+
+import SwiftUI
+
+@main
+struct GroupProjectIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
