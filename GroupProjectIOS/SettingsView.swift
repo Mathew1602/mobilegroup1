d64dc8683e502c7010cd9dcf901820090e9fabbd
Xiaoya Zou <84748829+xiaoyazz@users.noreply.github.com>
2024-11-06 23:51:25 -0500

    Update tabview nav bar in GroupProjectIOSApp file

---
+++ GroupProjectIOS/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  GroupProjectIOS
+//
+//  Created by Xiaoya Zou on 2024-11-06.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
