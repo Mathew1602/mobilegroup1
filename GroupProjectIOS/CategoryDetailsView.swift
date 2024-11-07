3a572b5f79b1f41868efcc1587b89bd052bfd29e
Xiaoya Zou <84748829+xiaoyazz@users.noreply.github.com>
2024-11-07 00:29:49 -0500

    Update CategoryCardView

---
+++ GroupProjectIOS/CategoryDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryDetailsView.swift
+//  GroupProjectIOS
+//
+//  Created by Xiaoya Zou on 2024-11-07.
+//
+
+import SwiftUI
+
+struct CategoryDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryDetailsView()
+}
---
+++ GroupProjectIOS/UpdateFoodView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateFoodView.swift
+//  GroupProjectIOS
+//
+//  Created by Xiaoya Zou on 2024-11-07.
+//
+
+import SwiftUI
+
+struct UpdateFoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateFoodView()
+}
