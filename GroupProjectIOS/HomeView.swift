9b937beb66afa271e26977aa8db06081e143a4d6
Xiaoya Zou <84748829+xiaoyazz@users.noreply.github.com>
2024-11-07 00:30:05 -0500

    Update CategoryDetailsView

--- GroupProjectIOS/HomeView.swift
+++ GroupProjectIOS/HomeView.swift
@@ -8,11 +8,77 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    // Initialize Food Categoruy ViewModel
+    @EnvironmentObject private var foodCategoryViewModel : FoodCategoryViewModel
+    @EnvironmentObject private var foodViewModel : FoodViewModel
+    @State private var searchText: String = ""
+    @State private var searchRes : [Food] = []
+    
+    // carousel images
+    let foodImages = ["food1", "food2", "food3"]
+    
+    private var filteredCategories: [FoodCategory] {
+        if searchText.isEmpty {
+            return foodCategoryViewModel.categories
+        } else {
+            return foodCategoryViewModel.categories.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+    
+    // For grid layout
+    let columns = [
+        GridItem(.fixed(170), spacing: 16),
+        GridItem(.fixed(170), spacing: 16)
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                
+                // carousel
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(foodImages, id: \.self) { imageName in
+                            Image(imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 250, height: 150) // Adjust width/height as needed
+                                .clipped()
+                                .cornerRadius(10)
+                                .shadow(radius: 4)
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.vertical, 10)
+                
+                // Grid layout
+                ScrollView {
+                    Spacer()
+                    LazyVGrid(columns: columns, spacing: 18) {
+                        ForEach(filteredCategories, id: \.id) { category in
+                            NavigationLink(destination: CategoryDetailsView(category: category)) {
+                                CategoryCardView(category: category)
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+            }
+            .navigationTitle("What's In My Fridge")
+            .onAppear {
+                foodCategoryViewModel.fetchCategories() // Fetch categories when HomeView appears
+                foodViewModel.requestNotificationPermission()
+            }
+        }
+        .searchable(text: $searchText,
+                    placement: .navigationBarDrawer(displayMode: .always)) // MUST have the placement attribute, or else the searchbar will disappear after navigation back from CategoryDetailsView!!!😠
     }
 }
 
 #Preview {
     HomeView()
+        .environmentObject(FoodCategoryViewModel())
+        .environmentObject(FoodViewModel())
 }
--- GroupProjectIOS/UpdateFoodView.swift
+++ GroupProjectIOS/UpdateFoodView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct UpdateFoodView: View {
+    
+    @EnvironmentObject var foodViewModel: FoodViewModel
+    @EnvironmentObject var foodCategoryViewModel: FoodCategoryViewModel
+    
+    @State var food: Food
+    @State var showAlert = false
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter
+    }()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationStack{
+            
+            VStack{
+                Form {
+                        
+                    TextField("Name", text: $food.name)
+                    
+                    Stepper(value: $food.quantity, in: 1...100) {
+                        Text("Quantity: \(food.quantity)")
+                    }
+                                
+                    DatePicker("Expiration Date", selection: $food.expirationDate, displayedComponents: .date)
+                            
+                    Picker("Category", selection: $food.category) {
+                        ForEach(foodCategoryViewModel.categories, id: \.self) { category in
+                            Text(category.name).tag(category.name)
+                        }
+                    }
+                    
+                    Toggle("Enable Expiration Notification", isOn: $foodViewModel.isNotificationEnabled)
+                        .padding(.vertical)
+                }
+                
+                Button("Save Changes"){
+                    foodViewModel.updateFood(food)
+                    showAlert = true
+                }
+                .frame(maxWidth: .infinity)
+                .padding() // For button text
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                .padding(.horizontal) // For button itself
+                .alert("Successfully Updated.", isPresented: $showAlert) {
+                    Button("OK", role: .cancel){}
+                }
+            }
+            .navigationTitle("\(food.name)")
+            .padding(.bottom, 20)
+            Spacer()
+            
+        }
     }
 }
 
 #Preview {
-    UpdateFoodView()
+    UpdateFoodView(food: Food(id: "sampleID", name: "Sample Food", quantity: 2, expirationDate: Date(), category: "Sample Category"))
+        .environmentObject(FoodViewModel())
+        .environmentObject(FoodCategoryViewModel())
 }
